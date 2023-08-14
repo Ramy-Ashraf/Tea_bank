aa2fd08dd3f01a6b27e3e3ae83ee9ba37bf5b9c3
Ramy-Ashraf <ramy.abdelmaseeh01@eng-st.cu.edu.eg>
2023-08-14 23:19:55 +0300

    session timeout postponed

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,42 @@
+
+
+// const sessionTimeoutDuration = 10000;
+// const cardDisplayDelay = 10000;
+
+// let sessionTimeout;
+// let cardTimeout;
+
+// function startSessionTimer() {
+//     sessionTimeout = setTimeout(showSessionTimeoutCard, sessionTimeoutDuration);
+// }
+
+// function resetSessionTimer() {
+//     clearTimeout(sessionTimeout);
+//     clearTimeout(cardTimeout);
+//     startSessionTimer();
+//     cardTimeout = setTimeout(showSessionTimeoutCard, cardDisplayDelay);
+// }
+
+// function showSessionTimeoutCard() {
+//     const card = document.getElementById("sessionTimeoutCard");
+//     card.style.display = "block";
+// }
+
+// function logout() {
+//     // logout logic
+// }
+
+// window.onload = function () {
+//     if (localStorage.getItem('islogin') === 'true') {
+//         startSessionTimer();
+//         cardTimeout = setTimeout(showSessionTimeoutCard, cardDisplayDelay);
+//         document.addEventListener("mousemove", resetSessionTimer);
+//         document.addEventListener("mousedown", resetSessionTimer);
+//         document.addEventListener("keypress", resetSessionTimer);
+//         document.addEventListener("touchstart", resetSessionTimer);
+//     }
+// };
+///////////////////////////////////////////////////////////////////////////////
 $(function () {
   $(document).scroll(function () {
     var $nav = $("#mainNavbar");
@@ -5,11 +44,7 @@ $(function () {
   });
 })
 
-
-
-
-
-
+/////////////////////////////////////////////////////////////////////////////////
 const form = document.querySelector('form.login');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
@@ -212,41 +247,5 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 ////////////////////////////////////////////////////////////////////////////
 
-const sessionTimeoutDuration = 10000;
-const cardDisplayDelay = 10000;
-
-let sessionTimeout;
-let cardTimeout;
-
-function startSessionTimer() {
-    sessionTimeout = setTimeout(showSessionTimeoutCard, sessionTimeoutDuration);
-}
-
-function resetSessionTimer() {
-    clearTimeout(sessionTimeout);
-    clearTimeout(cardTimeout);
-    startSessionTimer();
-    cardTimeout = setTimeout(showSessionTimeoutCard, cardDisplayDelay);
-}
-
-function showSessionTimeoutCard() {
-    const card = document.getElementById("sessionTimeoutCard");
-    card.style.display = "block";
-}
-
-function logout() {
-    // logout logic
-}
-
-window.onload = function () {
-    if (localStorage.getItem('islogin') === 'true') {
-        startSessionTimer();
-        cardTimeout = setTimeout(showSessionTimeoutCard, cardDisplayDelay);
-        document.addEventListener("mousemove", resetSessionTimer);
-        document.addEventListener("mousedown", resetSessionTimer);
-        document.addEventListener("keypress", resetSessionTimer);
-        document.addEventListener("touchstart", resetSessionTimer);
-    }
-};
 
 
