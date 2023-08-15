84dc341e9113523cd5e4ec9983262a73340355b0
Ramy-Ashraf <ramy.abdelmaseeh01@eng-st.cu.edu.eg>
2023-08-15 13:04:09 +0300

    file organizing

---
+++ scripts/timeout.js
@@ -0,0 +1,39 @@
+const sessionTimeoutDuration = 10000;
+const cardDisplayDelay = 10000;
+
+let sessionTimeout;
+let cardTimeout;
+
+function startSessionTimer() {
+  sessionTimeout = setTimeout(showSessionTimeoutCard, sessionTimeoutDuration);
+}
+
+function resetSessionTimer() {
+  clearTimeout(sessionTimeout);
+  clearTimeout(cardTimeout);
+  startSessionTimer();
+  cardTimeout = setTimeout(showSessionTimeoutCard, cardDisplayDelay);
+}
+
+function showSessionTimeoutCard() {
+  const card = document.getElementById("sessionTimeoutCard");
+  card.style.display = "block";
+}
+
+function logout() {
+  localStorage.setItem('islogin', 'false');
+  localStorage.removeItem('firstname');
+  localStorage.removeItem('lastname');
+  window.location.reload();
+}
+
+window.onload = function () {
+  if (localStorage.getItem('islogin') === 'true') {
+    startSessionTimer();
+    cardTimeout = setTimeout(showSessionTimeoutCard, cardDisplayDelay);
+    document.addEventListener("mousemove", resetSessionTimer);
+    document.addEventListener("mousedown", resetSessionTimer);
+    document.addEventListener("keypress", resetSessionTimer);
+    document.addEventListener("touchstart", resetSessionTimer);
+  }
+};
