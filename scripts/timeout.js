ddc34086f01bb9a2637050aae8a010bf94ce5b67
Ramy-Ashraf <ramy.abdelmaseeh01@eng-st.cu.edu.eg>
2023-08-16 11:33:57 +0300

    done

--- scripts/timeout.js
+++ scripts/timeout.js
@@ -21,11 +21,11 @@ function showSessionTimeoutCard() {
 }
 
 function logout() {
+  // window.location.href = "../pages/index.html";
   localStorage.setItem('islogin', 'false');
   localStorage.removeItem('firstname');
   localStorage.removeItem('lastname');
-  window.location.href = "../pages/index.html";
-  window.location.reload();
+  window.location.href='../pages/index.html';
 }
 
 window.onload = function () {
