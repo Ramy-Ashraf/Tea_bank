4c953ac80bbc654f7cda21ef42b0d652b6140a52
Malak Ashraf <malak.hefny@student.guc.edu.eg>
2023-08-16 10:42:48 +0200

    m

--- scripts/register.js
+++ scripts/register.js
@@ -93,14 +93,14 @@ form.addEventListener("submit", e => {
             islogin = true;
             localStorage.setItem("firstname", fnameField.value);
             localStorage.setItem("islogin", islogin);
-            window.location.href = "Home.html";
+            window.location.href = "index.html";
         }
     } else {
         if (fnameField.parentElement.classList.contains("success") && passField.parentElement.classList.contains("success")) {
             islogin = true;
             localStorage.setItem("firstname", fnameField.value);
             localStorage.setItem("islogin", islogin);
-            window.location.href = "Home.html";
+            window.location.href = "index.html";
         }
     }
 
